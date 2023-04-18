6c5f6778021ce7724da036d7bc080a2408bc6ecd
Hernan . Sanchez <hernan.sanchez@MUSALA.COM>
2023-04-18 10:35:56 +0300

    Lesson 8

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,37 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("App starts...");
+        LOGGER.info(Arrays.toString(this.twitterToKafkaServiceConfigData.getTwitterKeywords().toArray()));
+        LOGGER.info(this.twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start();
+    }
+}
---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/listener/TwitterKafkaStatusListener.java
@@ -0,0 +1,17 @@
+package com.microservices.demo.twitter.to.kafka.service.listener;
+
+import com.microservices.demo.twitter.to.kafka.service.TwitterToKafkaServiceApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import twitter4j.Status;
+import twitter4j.StatusAdapter;
+
+@Component
+public class TwitterKafkaStatusListener extends StatusAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    @Override
+    public void onStatus(Status status) {
+        LOGGER.info("Twitter status with text {}", status.getText());
+    }
+}
