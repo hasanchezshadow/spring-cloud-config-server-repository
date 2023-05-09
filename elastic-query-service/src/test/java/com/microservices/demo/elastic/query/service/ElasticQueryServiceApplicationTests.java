b9e3b5f5f66bc7ddc5832484686b48418952387b
Hernan.Sanchez <hernan.sanchez@musala.com>
2023-05-09 10:58:51 +0300

    [Section 7]   Lesson 78     Calling the Kafka State Store from Query Service

--- elastic-query-service/src/test/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplicationTests.java
+++ elastic-query-service/src/test/java/com/microservices/demo/elastic/query/service/ElasticQueryServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ElasticQueryServiceApplicationTests {
-    @Test
-    public void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 }
