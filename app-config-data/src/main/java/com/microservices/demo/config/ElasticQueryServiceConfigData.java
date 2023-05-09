2f0fdfe3f201ad49946cccb867e99b470745423b
Hernan.Sanchez <hernan.sanchez@musala.com>
2023-05-09 10:56:40 +0300

    [Section 7]   Lesson 78     Calling the Kafka State Store from Query Service

--- app-config-data/src/main/java/com/microservices/demo/config/ElasticQueryServiceConfigData.java
+++ app-config-data/src/main/java/com/microservices/demo/config/ElasticQueryServiceConfigData.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "elastic-query-service")
@@ -13,4 +11,24 @@ public class ElasticQueryServiceConfigData {
     private String version;
     private String customAudience;
     private Long backPressureDelayMs;
+    private WebClient webClient;
+    private Query queryFromKafkaStateStore;
+
+    @Data
+    public static class WebClient {
+        private Integer connectTimeoutMs;
+        private Integer readTimeoutMs;
+        private Integer writeTimeoutMs;
+        private Integer maxInMemorySize;
+        private String contentType;
+        private String acceptType;
+        private String queryType;
+    }
+
+    @Data
+    public static class Query {
+        private String method;
+        private String accept;
+        private String uri;
+    }
 }
